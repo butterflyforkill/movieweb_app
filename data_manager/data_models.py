ace27b5f59e215158d2046362cb4bb520e49ea96
butterflyforkill <vasse.paul@gmail.com>
2024-08-30 15:23:53 +0200

    Add data models

--- data_manager/data_models.py
+++ data_manager/data_models.py
@@ -1,2 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_name = db.Column(db.String(100), nullable = False)
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    movie_name = db.Column(db.String(250), nullable = False)
+    movie_director = db.Column(db.String(100), nullable = False)
+    release_year = db.Column(db.Integer, nullable = False)
+    movie_rating = db.Column(db.Float, nullable = False)
--- data_manager/sqlite_data_manager.py
+++ data_manager/sqlite_data_manager.py
@@ -1,5 +1,6 @@
 from data_manager_interface import DataManagerInterface
 from flask_sqlalchemy import SQLAlchemy
+from data_models import Movie, User
 
 class SQLiteDataManager(DataManagerInterface):
     def __init__(self, db_file_name):
